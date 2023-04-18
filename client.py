310bdcf5fda8c88e7f0c4c85bea26d1ab6eb39f0
AlexandrReznik <saha.reznik2004@gmail.com>
2023-04-18 15:50:09 +0300

    Log decorator

--- client.py
+++ client.py
@@ -3,21 +3,39 @@ import socket
 import sys
 import log.client_log_config
 import logging
+import datetime
+import inspect
+
+
+def log(func):
+    def wrapper(*args, **kwargs):
+        calling_func = inspect.stack()[1][3]
+        now = datetime.datetime.now()
+        log_string = f'{now} Function {func.__name__} called from function {calling_func}'
+        print(log_string)
+        with open('log.txt', 'a') as log_file:
+            log_file.write(log_string + '\n')
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
 
 
 CLIENT_LOGGER = logging.getLogger('client')
 
 
+@log
 def send_message(sock, message):
     encoded_message = json.dumps(message).encode('utf-8')
     sock.send(encoded_message)
 
 
+@log
 def receive_message(sock):
     data = sock.recv(1024)
     return json.loads(data.decode('utf-8'))
 
 
+@log
 def create_presence_message(account_name):
     message = {
         'action': 'presence',
@@ -30,6 +48,7 @@ def create_presence_message(account_name):
     return message
 
 
+@log
 def parse_server_response(response):
     if 'response' in response:
         return response['response']
--- server.py
+++ server.py
@@ -3,21 +3,39 @@ import socket
 import sys
 import log.server_log_config
 import logging
+import datetime
+import inspect
+
+
+def log(func):
+    def wrapper(*args, **kwargs):
+        calling_func = inspect.stack()[1][3]
+        now = datetime.datetime.now()
+        log_string = f'{now} Function {func.__name__} called from function {calling_func}'
+        print(log_string)
+        with open('log.txt', 'a') as log_file:
+            log_file.write(log_string + '\n')
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
 
 
 SERVER_LOGGER = logging.getLogger('server')
 
 
+@log
 def send_message(sock, message):
     encoded_message = json.dumps(message).encode('utf-8')
     sock.send(encoded_message)
 
 
+@log
 def receive_message(sock):
     data = sock.recv(1024)
     return json.loads(data.decode('utf-8'))
 
 
+@log
 def handle_presence_message(message):
     response = {
         'response': 200,
@@ -27,6 +45,7 @@ def handle_presence_message(message):
     return response
 
 
+@log
 def handle_client_message(sock, message):
     action = message.get('action')
     if action == 'presence':
